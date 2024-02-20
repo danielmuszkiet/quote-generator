b8ffb4b2180d297efa8e42e344d8598e54dde664
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2024-02-20 17:10:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const radomIndex = Math.floor(Math.random() * apiQuotes.length);
+  const quote = apiQuotes[radomIndex];
+
+  authorText.innerHTML = quote.author == "Anonymous" ? "Unknown" : quote.author;
+
+  // Check Quote Length to etermine styling
+  if (quote.text.length > 80) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// GET Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    alert(error.message);
+  }
+}
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
